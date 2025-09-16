752327d24a9134e674e9fbe1465e61939fac2e8b
Hinrick <hinrick71@gmail.com>
2025-09-16 10:28:52 +0800

    change production url

--- src/pages/interactive_art/index.tsx
+++ src/pages/interactive_art/index.tsx
@@ -33,21 +33,21 @@ export default function ReportDownload() {
             <div className={style.photo_area}>
               <div>
                 <CardWithMoreEffect
-                  image_src="https://ctspcsr.com.tw/meida/award_photo/1/1.jpg"
+                  image_src="https://sd.ctsp.gov.tw/meida/award_photo/1/1.jpg"
                   alt="team"
                   text="卓錫隆_綠能科技遇到親子遊(銀獎)"
                 />
               </div>
               <div>
                 <CardWithMoreEffect
-                  image_src="https://ctspcsr.com.tw/meida/award_photo/1/2.jpg"
+                  image_src="https://sd.ctsp.gov.tw/meida/award_photo/1/2.jpg"
                   alt="team"
                   text="李祐村_台中園區之美13(銅獎)"
                 />
               </div>
               <div>
                 <CardWithMoreEffect
-                  image_src="https://ctspcsr.com.tw/meida/award_photo/1/3.jpg"
+                  image_src="https://sd.ctsp.gov.tw/meida/award_photo/1/3.jpg"
                   alt="team"
                   text="趙令級_科技與休閒(首獎)"
                 />
@@ -62,77 +62,77 @@ export default function ReportDownload() {
               <div className={style.photo_area}>
                 <div>
                   <CardWithMoreEffect
-                    image_src="https://ctspcsr.com.tw/meida/award_photo/2/1.jpg"
+                    image_src="https://sd.ctsp.gov.tw/meida/award_photo/2/1.jpg"
                     alt="team"
                     text="張凱智_中科管理局倒影之美(佳作)"
                   />
                 </div>
                 <div>
                   <CardWithMoreEffect
-                    image_src="https://ctspcsr.com.tw/meida/award_photo/2/2.jpg"
+                    image_src="https://sd.ctsp.gov.tw/meida/award_photo/2/2.jpg"
                     alt="team"
                     text="戴孟佳_幸福中科(佳作)"
                   />
                 </div>
                 <div>
                   <CardWithMoreEffect
-                    image_src="https://ctspcsr.com.tw/meida/award_photo/2/3.jpg"
+                    image_src="https://sd.ctsp.gov.tw/meida/award_photo/2/3.jpg"
                     alt="team"
                     text="李振源_晨曦之美(佳作)"
                   />
                 </div>
                 <div>
                   <CardWithMoreEffect
-                    image_src="https://ctspcsr.com.tw/meida/award_photo/2/4.jpg"
+                    image_src="https://sd.ctsp.gov.tw/meida/award_photo/2/4.jpg"
                     alt="team"
                     text="柯月娥_參觀南投中科(佳作)"
                   />
                 </div>
                 <div>
                   <CardWithMoreEffect
-                    image_src="https://ctspcsr.com.tw/meida/award_photo/2/5.jpg"
+                    image_src="https://sd.ctsp.gov.tw/meida/award_photo/2/5.jpg"
                     alt="team"
                     text="沈朝財_中科16週年慶(佳作)"
                   />
                 </div>
                 <div>
                   <CardWithMoreEffect
-                    image_src="https://ctspcsr.com.tw/meida/award_photo/2/6.jpg"
+                    image_src="https://sd.ctsp.gov.tw/meida/award_photo/2/6.jpg"
                     alt="team"
                     text="葉世賢_中科園區夜色之美(佳作)"
                   />
                 </div>
                 <div>
                   <CardWithMoreEffect
-                    image_src="https://ctspcsr.com.tw/meida/award_photo/2/7.jpg"
+                    image_src="https://sd.ctsp.gov.tw/meida/award_photo/2/7.jpg"
                     alt="team"
                     text="葉世賢_中科藝術之美(佳作)"
                   />
                 </div>
                 <div>
                   <CardWithMoreEffect
-                    image_src="https://ctspcsr.com.tw/meida/award_photo/2/8.jpg"
+                    image_src="https://sd.ctsp.gov.tw/meida/award_photo/2/8.jpg"
                     alt="team"
                     text="蔡承運_中科藍寶石(佳作)"
                   />
                 </div>
                 <div>
                   <CardWithMoreEffect
-                    image_src="https://ctspcsr.com.tw/meida/award_photo/2/9.jpg"
+                    image_src="https://sd.ctsp.gov.tw/meida/award_photo/2/9.jpg"
                     alt="team"
                     text="蘇華芳_彩色樹花(佳作)"
                   />
                 </div>
                 <div>
                   <CardWithMoreEffect
-                    image_src="https://ctspcsr.com.tw/meida/award_photo/2/10.jpg"
+                    image_src="https://sd.ctsp.gov.tw/meida/award_photo/2/10.jpg"
                     alt="team"
                     text="趙令級_一起加油(佳作)"
                   />
                 </div>
                 <div>
                   <CardWithMoreEffect
-                    image_src="https://ctspcsr.com.tw/meida/award_photo/2/11.jpg"
+                    image_src="https://sd.ctsp.gov.tw/meida/award_photo/2/11.jpg"
                     alt="team"
                     text="陳建興_上班去(佳作)"
                   />
@@ -148,105 +148,105 @@ export default function ReportDownload() {
               <div className={style.photo_area}>
                 <div>
                   <CardWithMoreEffect
-                    image_src="https://ctspcsr.com.tw/meida/award_photo/3/1.jpg"
+                    image_src="https://sd.ctsp.gov.tw/meida/award_photo/3/1.jpg"
                     alt="team"
                     text="林國勛_騎上金色山脈的巨輪(中科)"
                   />
                 </div>
                 <div>
                   <CardWithMoreEffect
-                    image_src="https://ctspcsr.com.tw/meida/award_photo/3/2.jpg"
+                    image_src="https://sd.ctsp.gov.tw/meida/award_photo/3/2.jpg"
                     alt="team"
                     text="王善立_正瀚生技景觀魚池(中科)"
                   />
                 </div>
                 <div>
                   <CardWithMoreEffect
-                    image_src="https://ctspcsr.com.tw/meida/award_photo/3/3.jpg"
+                    image_src="https://sd.ctsp.gov.tw/meida/award_photo/3/3.jpg"
                     alt="team"
                     text="張瑞福_火燒晨光(圖文)"
                   />
                 </div>
                 <div>
                   <CardWithMoreEffect
-                    image_src="https://ctspcsr.com.tw/meida/award_photo/3/4.jpg"
+                    image_src="https://sd.ctsp.gov.tw/meida/award_photo/3/4.jpg"
                     alt="team"
                     text="王進明_美光東遊記(圖文)"
                   />
                 </div>
                 <div>
                   <CardWithMoreEffect
-                    image_src="https://ctspcsr.com.tw/meida/award_photo/3/5.jpg"
+                    image_src="https://sd.ctsp.gov.tw/meida/award_photo/3/5.jpg"
                     alt="team"
                     text="黃春麗_夕陽美景照映台中園區(二)(圖文)"
                   />
                 </div>
                 <div>
                   <CardWithMoreEffect
-                    image_src="https://ctspcsr.com.tw/meida/award_photo/3/6.jpg"
+                    image_src="https://sd.ctsp.gov.tw/meida/award_photo/3/6.jpg"
                     alt="team"
                     text="戴孟佳_迎向幸福中科(婚紗)"
                   />
                 </div>
                 <div>
                   <CardWithMoreEffect
-                    image_src="https://ctspcsr.com.tw/meida/award_photo/3/7.jpg"
+                    image_src="https://sd.ctsp.gov.tw/meida/award_photo/3/7.jpg"
                     alt="team"
                     text="李靜宜_我們結婚吧!(婚紗)"
                   />
                 </div>
                 <div>
                   <CardWithMoreEffect
-                    image_src="https://ctspcsr.com.tw/meida/award_photo/3/8.jpg"
+                    image_src="https://sd.ctsp.gov.tw/meida/award_photo/3/8.jpg"
                     alt="team"
                     text="唐蔚光_發現中科之美(景觀)"
                   />
                 </div>
                 <div>
                   <CardWithMoreEffect
-                    image_src="https://ctspcsr.com.tw/meida/award_photo/3/9.jpg"
+                    image_src="https://sd.ctsp.gov.tw/meida/award_photo/3/9.jpg"
                     alt="team"
                     text="李茂楓_夕彩2(景觀)"
                   />
                 </div>
                 <div>
                   <CardWithMoreEffect
-                    image_src="https://ctspcsr.com.tw/meida/award_photo/3/10.jpg"
+                    image_src="https://sd.ctsp.gov.tw/meida/award_photo/3/10.jpg"
                     alt="team"
                     text="趙令級_要科技也要美麗(景觀)"
                   />
                 </div>
                 <div>
                   <CardWithMoreEffect
-                    image_src="https://ctspcsr.com.tw/meida/award_photo/3/11.jpg"
+                    image_src="https://sd.ctsp.gov.tw/meida/award_photo/3/11.jpg"
                     alt="team"
                     text="陳光宏_創新園區之美(景觀)"
                   />
                 </div>
                 <div>
                   <CardWithMoreEffect
-                    image_src="https://ctspcsr.com.tw/meida/award_photo/3/12.jpg"
+                    image_src="https://sd.ctsp.gov.tw/meida/award_photo/3/12.jpg"
                     alt="team"
                     text="黃春麗_台中園區大眾運輸接駁站(一)(景觀)"
                   />
                 </div>
                 <div>
                   <CardWithMoreEffect
-                    image_src="https://ctspcsr.com.tw/meida/award_photo/3/13.jpg"
+                    image_src="https://sd.ctsp.gov.tw/meida/award_photo/3/13.jpg"
                     alt="team"
                     text="趙令級_要科技也要美麗(景觀)"
                   />
                 </div>
                 <div>
                   <CardWithMoreEffect
-                    image_src="https://ctspcsr.com.tw/meida/award_photo/3/14.jpg"
+                    image_src="https://sd.ctsp.gov.tw/meida/award_photo/3/14.jpg"
                     alt="team"
                     text="陳光宏_創新園區之美(景觀)"
                   />
                 </div>
                 <div>
                   <CardWithMoreEffect
-                    image_src="https://ctspcsr.com.tw/meida/award_photo/3/15.jpg"
+                    image_src="https://sd.ctsp.gov.tw/meida/award_photo/3/15.jpg"
                     alt="team"
                     text="黃春麗_台中園區大眾運輸接駁站(一)(景觀)"
                   />
@@ -262,56 +262,56 @@ export default function ReportDownload() {
               <div className={style.photo_area}>
                 <div>
                   <CardWithMoreEffect
-                    image_src="https://ctspcsr.com.tw/meida/award_photo/4/1.jpg"
+                    image_src="https://sd.ctsp.gov.tw/meida/award_photo/4/1.jpg"
                     alt="team"
                     text="Jimmy P. NERA_Refflectd Light City Lights(入選獎)"
                   />
                 </div>
                 <div>
                   <CardWithMoreEffect
-                    image_src="https://ctspcsr.com.tw/meida/award_photo/4/2.jpg"
+                    image_src="https://sd.ctsp.gov.tw/meida/award_photo/4/2.jpg"
                     alt="team"
                     text="Marana Frane Josef Balut_wedding(入選獎)"
                   />
                 </div>
                 <div>
                   <CardWithMoreEffect
-                    image_src="https://ctspcsr.com.tw/meida/award_photo/4/3.jpg"
+                    image_src="https://sd.ctsp.gov.tw/meida/award_photo/4/3.jpg"
                     alt="team"
                     text="曹加和_相印成趣(入選獎)"
                   />
                 </div>
                 <div>
                   <CardWithMoreEffect
-                    image_src="https://ctspcsr.com.tw/meida/award_photo/4/4.jpg"
+                    image_src="https://sd.ctsp.gov.tw/meida/award_photo/4/4.jpg"
                     alt="team"
                     text="林建宏_夢想園地(入選獎)"
                   />
                 </div>
                 <div>
                   <CardWithMoreEffect
-                    image_src="https://ctspcsr.com.tw/meida/award_photo/4/5.jpg"
+                    image_src="https://sd.ctsp.gov.tw/meida/award_photo/4/5.jpg"
                     alt="team"
                     text="林泊汶_逆游而上(入選獎)"
                   />
                 </div>
                 <div>
                   <CardWithMoreEffect
-                    image_src="https://ctspcsr.com.tw/meida/award_photo/4/6.jpg"
+                    image_src="https://sd.ctsp.gov.tw/meida/award_photo/4/6.jpg"
                     alt="team"
                     text="蔡豐吉_延伸(入選獎)"
                   />
                 </div>
                 <div>
                   <CardWithMoreEffect
-                    image_src="https://ctspcsr.com.tw/meida/award_photo/4/7.jpg"
+                    image_src="https://sd.ctsp.gov.tw/meida/award_photo/4/7.jpg"
                     alt="team"
                     text="陳仕豪_守護(入選獎)"
                   />
                 </div>
                 <div>
                   <CardWithMoreEffect
-                    image_src="https://ctspcsr.com.tw/meida/award_photo/4/8.jpg"
+                    image_src="https://sd.ctsp.gov.tw/meida/award_photo/4/8.jpg"
                     alt="team"
                     text="陳萬教_卑微處有大美(入選獎)"
                   />
